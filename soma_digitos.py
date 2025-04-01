872885ec3505ddf8ac72dce73a4db64e87a025b8
VinnyVSS <vinis8139@gmail.com>
2025-04-01 19:48:57 -0300

    Update soma_digitos.py

--- soma_digitos.py
+++ soma_digitos.py
@@ -1,7 +1,7 @@
 def soma_digitos(n):
     if n < 10:
-        return n  # Caso base: número com um único dígito
-    return (n % 10) + soma_digitos(n // 10)  # Soma o último dígito e chama recursivamente
+        return n 
+    return (n % 10) + soma_digitos(n // 10)  
 
 numero = int(input("Digite um número inteiro: "))
 print(f"Soma dos dígitos: {soma_digitos(numero)}")
